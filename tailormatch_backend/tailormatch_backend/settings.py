a4bfb167fae4a5bf2a89bd4121a21486ad14c571
Acer <acer@example.com>
2026-07-13 00:31:08 +0800

    Switch TailorMatch database to MySQL in XAMPP

--- tailormatch_backend/tailormatch_backend/settings.py
+++ tailormatch_backend/tailormatch_backend/settings.py
@@ -84,8 +84,12 @@ ASGI_APPLICATION = 'tailormatch_backend.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tailormatch_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
